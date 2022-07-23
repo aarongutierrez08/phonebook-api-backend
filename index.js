1146dcfe6311dfa54f0eaf2e360fc89b64804bfd
Aaron <aarongastongutierrez@gmail.com>
2022-07-22 23:10:30 -0300

    part 3 exercise 3.14

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body))
@@ -9,75 +11,36 @@ app.use(morgan(':method :url :status :res[content-length] :response-time ms :bod
 app.use(cors())
 app.use(express.static('build'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mar Poppendick",
-    number: "39-23-6423122"
-  }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>PHONEBOOK</h1>')
 })
   
 app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
     response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+  Person.findById(request.params.id).then(person => {
+    response.json(person)
+  })
 })
 
 app.get('/info', (request, response) => {
-    response.send(
-      `<p>Phonebook has info for ${persons.length} people</p>
-       <p>${new Date()}</p>`
-      )
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+    )
 })
-
+/*
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
 })
-
-/*
-const generateId = () => {
-    const maxId = persons.length > 0
-      ? Math.max(...persons.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
 */
-
-function random(min, max) {
-    return Math.floor((Math.random() * (max - min + 1)) + min);
-}
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
   
@@ -85,26 +48,29 @@ app.post('/api/persons', (request, response) => {
       return response.status(400).json({ 
         error: 'name missing' 
       })
-    } else if (persons.find(p => p.name === body.name)) {
+    }
+    /*
+    else if (persons.find(p => p.name === body.name)) {
       return response.status(400).json({ 
         error: 'name must be unique'
       })
-    } else if (!body.number) {
+    } 
+    */
+    else if (!body.number) {
       return response.status(400).json({ 
         error: 'number missing' 
       })
     }
 
-    const person = {
-      id: random(4, 1000),
+    const person = new Person({
       name: body.name,
       number: body.number
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(person)
-  })
+    })
+
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+})    
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Person = require('./models/person')
+
+if (process.argv.length == 3 || process.argv.length > 4){
+  console.log('Please provide the name and number as arguments: node mongo.js <name> <number>')
+  process.exit(1)
+}
+
+if (process.argv.length == 2) {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
+const newName = process.argv[2]
+const newNumber = process.argv[3]
+
+const newPerson = new Person({
+    name: newName,
+    number: newNumber
+})
+
+if (process.argv.length == 4) {
+
+    newPerson.save().then(result => {
+        console.log(result)
+        console.log(`added ${newName} with number ${newNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+} 
+
